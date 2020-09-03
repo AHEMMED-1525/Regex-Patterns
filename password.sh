300be1f85e999bd078fe1427d38afde9a19c601c
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-09-03 15:51:02 +0530

    Password contains min 8 characters

---
+++ password.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+read -p "Enter your password " password
+rejex=^[a-z]{8}$
+if [[ $password =~ $rejex ]]
+then
+	echo "$password is a valid password"
+else
+	echo "Please Enter the password contains min 8 characters"
+fi
+
