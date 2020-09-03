aa6aca9d733d13679b91e946fec39e1162cd96d4
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-09-03 15:55:54 +0530

    password with one Upper case

---
+++ passUppercase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+read -p "Enter your paswword " password
+rejex=^[A-Z]{1}[a-z]{7}$
+if [[ $password =~ $rejex ]]
+then
+	echo "The valid password $password"
+else
+	echo "Please Enter the password atleast one Uppercase with min 8 char"
+fi
+
